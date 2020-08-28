b8b88690a9311d84857232748dc1c9948826bb94
elhadjiomar <simbadeveloppement@gmail.com>
2020-08-28 10:51:07 +0100

    initial commit

---
+++ src/main/java/sn/uvs/courses/SpringMvcFoodDeliveryApplication.java
@@ -0,0 +1,13 @@
+package sn.uvs.courses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcFoodDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcFoodDeliveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sn/uvs/courses/SpringMvcFoodDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.uvs.courses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcFoodDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
